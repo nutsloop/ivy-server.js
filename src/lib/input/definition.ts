752674624b329994eccc0c1f3b2e19e68b385546
Simone Del Popolo <simonedelpopolo@outlook.com>
2024-05-22 10:59:22 +0000

    updated version

--- src/lib/input/definition.ts
+++ src/lib/input/definition.ts
@@ -15,7 +15,7 @@ export async function definition(): Promise<void> {
     name: 'ivy-server',
     npmjs: 'https://www.npmjs.com/settings/ivy-industries/packages',
     usage: 'ivy-server [--global-flag=[options]] <command> [--flag=[options]]',
-    version: '1.0.0-alpha.10',
+    version: '1.0.0-alpha.11',
     website: 'https://server.ivy.run'
   } );
 
