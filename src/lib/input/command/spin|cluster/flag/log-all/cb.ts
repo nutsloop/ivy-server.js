826628c6e0bd3bb73e1f977e446baaaf3446c062
Simone Del Popolo <simonedelpopolo@outlook.com>
2025-04-02 20:18:35 +0000

    Add TODOs for log-all flag
    Add comments indicating future changes.
    - Marked flag for removal with TODO.
    - Marked routing entry for removal with TODO.

--- src/lib/input/command/spin|cluster/flag/log-all/cb.ts
+++ src/lib/input/command/spin|cluster/flag/log-all/cb.ts
@@ -2,8 +2,10 @@ import { CallBackFlag } from '@nutsloop/ivy-input';
 
 import { routing } from '../../../../../server/routing.js';
 
+// TODO: removing this flag and logging every file requested.
 export const log_all_cb: CallBackFlag = (): void => {
 
+  // TODO: removing this entry in the routing Map
   routing.set( 'log-all', true );
   routing.set( 'log', true );
 };
