e3c48084aceff34f0969da8c40b6da3cfbe51e5b
Simone Del Popolo <simonedelpopolo@outlook.com>
2025-04-03 17:59:11 +0000

    Update log worker logic
    Refactor and enhance logging worker functionality.
    
    - Import cluster module to handle workers
    - Set process title with worker ID
    - Remove argument validation code
    - Add message handler for logging data
    - Include counter to track messages

--- src/lib/log/wrk.ts
+++ src/lib/log/wrk.ts
@@ -1,9 +1,14 @@
 #!/usr/bin/env -S node
+import cluster from 'node:cluster';
 
 process.argv.splice( 0, 2 );
+process.title = `ivy-log(${ cluster.worker.id })`;
 
-// TODO: work on the log worker
-if( process.argv.length > 2 ){
-  process.stderr.write( 'Too many arguments.\n' );
-  process.exit( 1 );
-}
+const counter: 1[] = [];
+process.on( 'message', ( data: { log_wrk:boolean, counter: number, worker_id: number, message: string[]} ) => {
+  counter.push( 1 );
+  process.stdout.write( `${data.message.join( ' ' )}` );
+  if( data.worker_id !== 0 ) {
+    process.stdout.write( `(${ data.worker_id })(${ data.counter.toString() })[${ counter.length.toString() }]\n` );
+  }
+} );
