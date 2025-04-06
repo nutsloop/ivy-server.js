1bc79019200f788c2384b6ecd8de385e4d0a0045
Simone Del Popolo <simonedelpopolo@outlook.com>
2025-04-06 19:21:57 +0000

    Add DomainConfig typing
    Updated Domain variable to use DomainConfig type.
    
    - Imported DomainConfig from '../index.ts'.
    - Changed Domain to explicitly use DomainConfig type.
    - Modified Domain.set parameters to include redirect_to.

--- src/example/multiDomainConfig.ts
+++ src/example/multiDomainConfig.ts
@@ -1,10 +1,10 @@
 
-import type { Routing, MultiDomainConfig } from '../index.ts';
+import type { Routing, MultiDomainConfig, DomainConfig, } from '../index.ts';
 
-const Domain = new Map();
-Domain.set( '0.0.0.0:3002', {
-  name: 'simonedelpopolo.com',
+const Domain: DomainConfig = new Map();
+Domain.set( [ '0.0.0.0:3002' ], {
   www_root: '0',
+  redirect_to: 'simonedelpopolo.com',
   redirect_to_https: false
 } );
 
