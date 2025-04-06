64cf22d1aa450c415cb9f613378da6ad25691bf5
Simone Del Popolo <simonedelpopolo@outlook.com>
2025-04-06 19:22:44 +0000

    Refactor multi-domain flag
    Simplify multi-domain file handling logic.
    
    - Remove unused `readFile` import.
    - Eliminate empty descriptor file validation.
    - Remove `domain_list` HTTPS redirect validation.

--- src/lib/input/command/spin|cluster/flag/multi-domain/cb.ts
+++ src/lib/input/command/spin|cluster/flag/multi-domain/cb.ts
@@ -1,6 +1,5 @@
 import { isFile, isValid } from '@nutsloop/ivy-cross-path';
 import { CallBackFlagAsync } from '@nutsloop/ivy-input';
-import { readFile } from 'node:fs/promises';
 
 import type { MultiDomainConfig } from '../../../../../server/dispacher.js';
 
@@ -23,25 +22,6 @@ export const multi_domain_cb: CallBackFlagAsync = async ( data: string ): Promis
     throw new Error( `--multi-domain path to file descriptor: [${multi_domain_file.message.red()}] is not valid.` );
   }
 
-  const read_domain_list: string = await readFile( multi_domain_file, { encoding: 'utf-8' } );
-  if( read_domain_list.length === 0 ) {
-
-    let message = `descriptor file must be filled with at least one domain.\n`;
-    message += 'it is a JSON object with a domain as key and a directory as value.\n';
-    message += `key pair of "domain.com" -> "directory".\n`;
-    message += `example:\n`;
-    message += `  {"domain1.com":"dir_domain1","domain2.com":"dir_domain2"}\n`;
-
-    throw new Error( `--multi-domain: [${multi_domain_file}] is empty.\n ${message}` );
-  }
-
   const multi_domain_config: MultiDomainConfig = ( await import( multi_domain_file ) ).default;
-
-  multi_domain_config.domain_list.forEach( ( domain ) => {
-    if( routing.get( 'secure' ) && domain.redirect_to_https ){
-      throw new Error( `redirect_to_https is not allowed when server is HTTPS already.` );
-    }
-  } );
-
   await multi_domain_config.callback( routing );
 };
