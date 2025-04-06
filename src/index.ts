d06d4dd5ec8ecac51e637408509c72a406b86d21
Simone Del Popolo <simonedelpopolo@outlook.com>
2025-04-06 19:22:11 +0000

    Add DomainConfig export
    Included DomainConfig in type exports.
    
    - Updated type export for MultiDomainConfig
    - Added DomainConfig to the export list

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ export type { ControlRoomConfig } from './lib/control/room.js';
 export type ServerResponse = RoutingServerResponse<RoutingIncomingMessage>;
 export type IncomingMessage = RoutingIncomingMessage;
 export type { LogConfig } from './lib/log/persistent.js';
-export type { MultiDomainConfig } from './lib/server/dispacher.js';
+export type { MultiDomainConfig, DomainConfig } from './lib/server/dispacher.js';
 export type { RequestData, Route, Routing } from './lib/server/routing.js';
 export { IvyServer } from './lib/server/server.js';
 export type { CreateServerOptions } from './lib/server/server.js';
