e1e515c633ec796c2d939c199966d477aaae165d
Simone Del Popolo <simonedelpopolo@outlook.com>
2025-04-19 17:23:04 +0000

    Bump version to 1.7.1-alpha.0
    Updated version references in package.json and definition.ts.
    
    - Changed version in package.json from 1.7.0-alpha.0 to 1.7.1-alpha.0
    - Updated version in definition.ts to match

--- src/lib/input/definition.ts
+++ src/lib/input/definition.ts
@@ -15,7 +15,7 @@ export async function definition(): Promise<void> {
     name: 'ivy-server',
     npmjs: 'https://www.npmjs.com/~nutsloop',
     usage: 'ivy-server [--global-flag=[options]] <command> [--flag=[options]]',
-    version: '1.7.0-alpha.0',
+    version: '1.7.1-alpha.0',
     website: 'https://github.com/sponsors/nutsloop'
   } );
 
