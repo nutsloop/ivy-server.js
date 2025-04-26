58912b0c4a9f95a65fc871e52bcc1d45f3d34579
Simone Del Popolo <simonedelpopolo@outlook.com>
2025-04-26 09:20:18 +0000

    Refactor listener function
    Renamed function and adjusted import path.
    - Changed function name to `on_message_listener`.
    - Updated import path for `routing`.

--- src/lib/process/on-message-listener.ts
+++ src/lib/process/on-message-listener.ts
@@ -1,8 +1,8 @@
 import { type Worker } from 'node:cluster';
 
-import { routing } from './routing.js';
+import { routing } from '../server/routing.js';
 
-export function process_listener( _Worker: Worker, data: { log_worker:boolean, counter: number, worker_id: number, message: string[]} ) {
+export function on_message_listener( _Worker: Worker, data: { log_worker:boolean, counter: number, worker_id: number, message: string[]} ) {
   if( data?.log_worker ){
     const log_worker = routing.get( 'log_worker' );
     if( log_worker ) {
