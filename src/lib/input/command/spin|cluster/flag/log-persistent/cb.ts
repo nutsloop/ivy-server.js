a8c9987028e773139c656cf5396025b7b42f1878
Simone Del Popolo <simonedelpopolo@outlook.com>
2025-04-26 09:22:39 +0000

    Refactor log_persistent_cb
    Remove unused parameter and adjust return type.
    
    - Eliminate `threads` parameter in `log_persistent_cb`.
    - Change return type from `number` to `void`.
    - Remove `cpus` import from `node:os`.

--- src/lib/input/command/spin|cluster/flag/log-persistent/cb.ts
+++ src/lib/input/command/spin|cluster/flag/log-persistent/cb.ts
@@ -1,11 +1,8 @@
 import { CallBackFlagAsync } from '@nutsloop/ivy-input';
-import { cpus } from 'node:os';
 
 import { routing } from '../../../../../server/routing.js';
 
-export const log_persistent_cb: CallBackFlagAsync<number> = async ( threads?: number ): Promise<number> => {
+export const log_persistent_cb: CallBackFlagAsync = async (): Promise<void> => {
 
   routing.set( 'log-persistent', true );
-
-  return threads || cpus().length;
 };
